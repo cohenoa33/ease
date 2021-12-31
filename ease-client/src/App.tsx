52d30cc4bf459f289f91d61a06ed37880848465a
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-12-31 11:43:28 -0800

    basic homepage template

--- ease-client/src/App.tsx
+++ ease-client/src/App.tsx
@@ -8,6 +8,9 @@ function App() {
         <div className="sub-title">WEB SUB TITLE. </div>
       </div>
       <div className="strip">
+        <img src="../gold-pond.jpg" alt="Nature" className="image" />
+      </div>
+      <div className="strip">
         <div className="box">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -18,6 +21,43 @@ function App() {
         <div className="box">
           Lorem Ipsum is simply dummy text of the printing and typesetting
         </div>
+      </div>
+      <div className="strip">
+        <div className="box">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like). It is a
+          long established fact that a reader will be distracted by the readable
+          content of a page when looking at its layout. The point of using Lorem
+          Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like
+          readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search
+          for 'lorem ipsum' will uncover many web sites still in their infancy.
+          Various versions have evolved over the years, sometimes by accident,
+          sometimes on purpose (injected humour and the like). It is a long
+          established fact that a reader will be distracted by the readable
+          content of a page when looking at its layout. The point of using Lorem
+          Ipsum is that it has a more-or-less normal distribution of letters, as
+          opposed to using 'Content here, content here', making it look like
+          readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search
+          for 'lorem ipsum' will uncover many web sites still in their infancy.
+          Various versions have evolved over the years, sometimes by accident,
+          sometimes on purpose (injected humour and the like).
+        </div>
+      </div>
+
+      <div className="app-footer">
+        <span className="footer-title"> NAME </span>
+        <span className="footer-text">WEB SUB TITLE. </span>
+        <div>Contact</div>
       </div>
     </div>
   );
