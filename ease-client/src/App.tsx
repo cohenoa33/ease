308dcad371412749fcb6262bc5727419cc2b277e
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-12-31 10:39:05 -0800

    basic css styling

---
+++ ease-client/src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="app-header">
+        <div className="title"> NAME </div>
+        <div className="sub-title">WEB SUB TITLE. </div>
+      </div>
+      <div className="strip">
+        <div className="box">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+        </div>
+        <div className="box">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+        </div>
+        <div className="box">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
