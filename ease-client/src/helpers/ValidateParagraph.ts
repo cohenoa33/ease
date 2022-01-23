a5f942973289ad8d6006996c3996633d3824a507
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2022-01-23 07:39:41 -0800

    update validte paragraph eneding function

--- ease-client/src/helpers/ValidateParagraph.ts
+++ ease-client/src/helpers/ValidateParagraph.ts
@@ -1,6 +1,6 @@
 export function validateParagraph(text: string): string {
   const validEndings = [".", "?", "!"];
   const lastChar = text.trim()[text.length - 1];
-  if (lastChar in validEndings) return text;
+  if (validEndings.includes(lastChar)) return text;
   return `${text.trim()}.`;
 }
