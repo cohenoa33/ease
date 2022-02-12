a46cc33ab70921d1a10c38383767afea0e05471c
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2022-02-12 12:40:41 -0800

    add hashrouter

--- ease-client/src/components/Navbar.tsx
+++ ease-client/src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
-
+import { HashRouter } from "react-router-dom";
 export function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <>
-      {open && <div className="open-bar">Navbar?</div>}
+      {open && (
+        <HashRouter>
+          <div className="open-bar">
+            <a href="#single-box">Single Box</a>
+          </div>
+        </HashRouter>
+      )}
       <div
         className={open ? `change navbar` : `navbar`}
         onClick={() => {
--- ease-client/src/components/strips/SingleBox.tsx
+++ ease-client/src/components/strips/SingleBox.tsx
@@ -5,11 +5,13 @@ import { titleCase } from "../../helpers";
 
 export function SingleBox({ text, title }: SingleBoxProps): ReactElement {
   return (
-    <Strip>
-      <div className="box">
-        <div className="box-title"> {title && titleCase(title)}</div>
-        <div className={"box-text"}> {text && titleCase(text, true)}</div>
-      </div>
-    </Strip>
+    <section id="single-box">
+      <Strip>
+        <div className="box">
+          <div className="box-title"> {title && titleCase(title)}</div>
+          <div className={"box-text"}> {text && titleCase(text, true)}</div>
+        </div>
+      </Strip>
+    </section>
   );
 }
